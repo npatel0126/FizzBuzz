4b23a90138588c3701a21233a5cb5d06303a3ac4
Neel Patel <npatell0126@gmail.com>
2025-09-08 16:36:07 -0400

    Add files via upload

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,20 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int count = multiples(1000, 3, 5);
+        System.out.println(count);
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int nums = 0;
+        for (int i = 1; i < n; i++){
+            boolean divA = i % a == 0;
+            boolean divB = i % b == 0;
+            if (divA || divB) {
+                nums++;
+            }
+        }
+        return nums;
+    }
+}
