fbcdfa6b7f67d70c55fa22eb0416dc8d57be51a6
Neel Patel <npatell0126@gmail.com>
2025-09-08 16:06:42 -0400

    Task 3.2

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -9,16 +9,13 @@ class FizzBuzz {
 
         //for (int i = 1; i < 100; i++) {
 
-        doFizzBuzz();
+        //doFizzBuzz();
     }
 
-    private static void doFizzBuzz() {
-        int i = 1;
-        while (i<100) {
-
+    public static void doFizzBuzz(int n) {
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            boolean divisibleBy3 = n % 3 == 0;
+            boolean divisibleBy5 = n % 5 == 0;
 
             // Print our appropriate result.
             if (divisibleBy3 && divisibleBy5) {
@@ -35,11 +32,7 @@ class FizzBuzz {
 
             } else {
 
-                System.out.println(i);
-
+                System.out.println(n);
             }
-            
-            i++;
-        }
     }
 }
